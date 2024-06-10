1e28d285b3df5387ea27fb392f4a5c2789364cae
Blaise deB Frederick <blaise.frederick@gmail.com>
2024-06-10 16:15:37 -0400

    Updated in line with splitrapidtide

--- micscriptlib/extractgms.py
+++ micscriptlib/extractgms.py
@@ -11,6 +11,12 @@ import sys
 
 import micscriptlib.util as micutil
 
+DEFAULT_NUMSECTIONS = 1
+DEFAULT_TASK = "rest"
+DEFAULT_SOURCETYPE = "HCP"
+DEFAULT_TIMELIMIT = "00:5:00"
+DEFAULT_MEM = "4G"
+
 fslmeantscmd = os.path.join(os.environ["FSLDIR"], "bin", "fslmeants")
 
 
@@ -27,7 +33,32 @@ def _get_parser():
         type=int,
         action="store",
         help="number of sections to split timecourse into (default is 2)",
-        default=2,
+        default=DEFAULT_NUMSECTIONS,
+    )
+    parser.add_argument(
+        "--timelimit",
+        metavar="TIME",
+        type=str,
+        action="store",
+        help=f"time limit of job in HH:MM:SS. Default is {DEFAULT_TIMELIMIT})",
+        default=DEFAULT_TIMELIMIT,
+    )
+    parser.add_argument(
+        "--mem",
+        metavar="MEM",
+        type=str,
+        action="store",
+        help=f"Memory limit of job in bytes.  Use the suffix M for megabytes, G for gigabytes. Default is {DEFAULT_MEM})",
+        default=DEFAULT_MEM,
+    )
+    parser.add_argument(
+        "--sourcetype",
+        dest="sourcetype",
+        action="store",
+        type=str,
+        choices=["HCP", "cole", "recig", "psusleep"],
+        help=f"Dataset (default is {DEFAULT_SOURCETYPE})",
+        default=DEFAULT_SOURCETYPE,
     )
     parser.add_argument(
         "--inputlist",
@@ -85,7 +116,7 @@ def _get_parser():
 ##########################################################################################
 
 
-def extractgms_workflow(sourcetype):
+def extractgms_workflow():
     global theoutputdir
     try:
         args = _get_parser().parse_args()
@@ -97,16 +128,16 @@ def extractgms_workflow(sourcetype):
         print(args)
 
     # file locations
-    outputroot = f"/data/frederic/{sourcetype}/derivatives"
-    if sourcetype == "cole":
+    outputroot = f"/data/frederic/{args.sourcetype}/derivatives"
+    if args.sourcetype == "cole":
         inputroot = "/data/ckorponay/New_HCP_Cleaned_TomMethod"
         theoutputdir = os.path.join(outputroot, "gms")
         thetypes = ["REST1", "REST2"]
-    elif sourcetype == "psusleep":
+    elif args.sourcetype == "psusleep":
         inputroot = "/data/ckorponay/Sleep/fmriprep"
         theoutputdir = os.path.join(outputroot, "gms")
         thetypes = ["rest", "sleep"]
-    elif sourcetype == "recig":
+    elif args.sourcetype == "recig":
         inputroot = "/data/ajanes/REcig/fmri"
         theoutputdir = os.path.join(outputroot, "gms")
         thetypes = ["cue1", "cue2", "cue3", "cue4", "cue5", "resting"]
@@ -118,7 +149,7 @@ def extractgms_workflow(sourcetype):
     SYSTYPE, SUBMITTER, SINGULARITY = micutil.getbatchinfo()
 
     if args.debug:
-        print(f"sourcetype is {sourcetype}")
+        print(f"sourcetype is {args.sourcetype}")
         print(f"\t{inputroot=}")
         print(f"\t{outputroot=}")
         print(f"\t{theoutputdir=}")
@@ -130,7 +161,7 @@ def extractgms_workflow(sourcetype):
     # loop over all run types
     for thetype in thetypes:
         # find the bold files
-        if sourcetype == "cole":
+        if args.sourcetype == "cole":
             theboldfiles = micutil.findboldfiles_cole(
                 inputroot,
                 thetype,
@@ -139,13 +170,13 @@ def extractgms_workflow(sourcetype):
                 inputlistfile=args.inputlistfile,
                 debug=args.debug,
             )
-        elif sourcetype == "psusleep":
+        elif args.sourcetype == "psusleep":
             theboldfiles = micutil.findboldfiles_psusleep(
                 thetype,
                 inputlistfile=args.inputlistfile,
                 debug=args.debug,
             )
-        elif sourcetype == "recig":
+        elif args.sourcetype == "recig":
             theboldfiles = micutil.findboldfiles_recig(
                 thetype,
                 inputlistfile=args.inputlistfile,
@@ -166,18 +197,18 @@ def extractgms_workflow(sourcetype):
             sys.exit(1)
 
         for thefile in theboldfiles:
-            if sourcetype == "cole":
+            if args.sourcetype == "cole":
                 absname, thesubj, therun, pedir = micutil.parseconnectomename(
                     thefile, volumeproc=args.volumeproc
                 )
                 maskname = os.path.join(therundir, "brainmask_fs.2.nii.gz")
-            elif sourcetype == "HCP":
+            elif args.sourcetype == "HCP":
                 absname, thesubj, therun, pedir = micutil.parseconnectomename(
                     thefile, volumeproc=args.volumeproc
                 )
                 outroot = os.path.join(thesubj, thesubj + "_" + thetype + "_" + pedir)
                 maskname = os.path.join(therundir, "brainmask_fs.2.nii.gz")
-            elif sourcetype == "psusleep":
+            elif args.sourcetype == "psusleep":
                 (
                     absname,
                     thefmrifilename,
@@ -192,7 +223,7 @@ def extractgms_workflow(sourcetype):
                 thetask = f"task-{thetask}"
                 outroot = os.path.join(thesubj, thesubj + "_" + thetask)
                 maskname = thefile.replace("desc-smoothAROMAnonaggr_bold", "res-2_desc-brain_mask")
-            elif sourcetype == "recig":
+            elif args.sourcetype == "recig":
                 absname, thefilename, thesubj, thesess, thetask = micutil.parserecigname(thefile)
                 thesubj = f"sub-{thesubj}"
                 thesess = f"ses-{thesess}"
@@ -244,7 +275,7 @@ def extractgms_workflow(sourcetype):
             thiscommand = thecommand + ["-o"]
             thiscommand.append(fulloutputname)
             scriptfile, thescript = micutil.make_runscript(
-                thiscommand, ncpus=1, jobname="extractgms"
+                thiscommand, timelimit=args.timelimit, mem=args.mem, ncpus=1, jobname="extractgms"
             )
             if dothis:
                 if args.doforreal:
@@ -255,42 +286,6 @@ def extractgms_workflow(sourcetype):
                     subprocess.call(sub_cmd)
                 else:
                     print(thescript)
-
-            """# before submitting the job, check to see if output file exists
-            startpoint = 20
-            endpoint = 1199
-            numpoints = endpoint - startpoint + 1
-            pointspersection = numpoints // args.numsections
-            for section in range(args.numsections):
-                sectionname = f"{section + 1}-of-{args.numsections}"
-                secstart = startpoint + section * pointspersection
-                secend = secstart + pointspersection - 1
-                inputrange = f"{secstart} {secend}"
-                outputname = f"{os.path.join(theoutputdir, outroot)}_{sectionname}.txt"
-                dothis = False
-                if args.existcheck:
-                    if os.path.isfile(outputname):
-                        print(outputname, "exists - skipping")
-                        dothis = False
-                    else:
-                        print(outputname, "does not exist - running")
-                        dothis = True
-                else:
-                    dothis = True
-    
-                thiscommand = thecommand + [f"--timerange {inputrange}", outputname]
-                thecommand.append(outputname)
-                scriptfile, thescript = micutil.make_runscript(thiscommand)
-                if dothis:
-                    if args.doforreal:
-                        if SYSTYPE == "sge":
-                            sub_cmd = f"{QSUB} -cwd -q fmriprep.q -N {args.jobname} -pe fmriprep 1 -w e -R y {filename}".split()
-                        elif SYSTYPE == "slurm":
-                            sub_cmd = f"{SBATCH} {scriptfile}".split()
-                            subprocess.call(sub_cmd)
-                    else:
-                        print(thescript)
-              """
 
 
 if __name__ == "__main__":
