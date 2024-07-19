d1ccc40b45b74e3641daaaebe9e3cdbd29084536
Blaise deB Frederick <blaise.frederick@gmail.com>
2024-07-19 16:16:26 -0400

    Update splitrapidtide.py

--- micscriptlib/splitrapidtide.py
+++ micscriptlib/splitrapidtide.py
@@ -386,7 +386,7 @@ def splitrapidtide_workflow():
                     print(f"{thespace=}")
                 outroot = os.path.join(
                     thesubj,
-                    thesubj + "_" + thesess + "_" +  thetask + "_" + outputnamesuffix + extrasuffix,
+                    thesubj + "_" + thesess + "_" +  thetask,
                 )
                 motionfile = None
                 designfile = None
